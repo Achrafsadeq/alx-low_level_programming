ca5a81d42ef73b9da622f7562de2c3f8de4f6943
Achrafsadeq <achrafsadeqq@gmail.com>
2024-06-20 12:15:14 -0700

    Add C program using puts to print a specific string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a message using the puts function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
