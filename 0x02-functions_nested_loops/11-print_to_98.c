a6612a66f39732d446301a3b2b0dc4526d642e9c
Achrafsadeq <achrafsadeqq@gmail.com>
2024-06-25 12:50:20 -0700

    Add function to print all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,29 +7,23 @@
  *
  * Description: This function prints all natural numbers from n to 98,
  *              in order, followed by a new line. Numbers are separated
- *              by a comma and a space. If the number is 98, it is followed
- *              by an exclamation mark.
+ *              by a comma and a space.
  */
 void print_to_98(int n)
 {
-	/* Print the current number and check if it is 98 */
-	if (n == 98)
+	if (n <= 98)
 	{
-		printf("98!\n");
-		return;
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
 	}
 	else
 	{
-		printf("%d, ", n);
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
 	}
-
-	/* Recursive call to move towards 98 */
-	if (n < 98)
-	{
-		print_to_98(n + 1);  /* Increment the number if it's less than 98 */
-	}
-	else
-	{
-		print_to_98(n - 1);  /* Decrement the number if it's greater than 98 */
-	}
+	printf("98\n");
 }
