55cbb4771e2c1694c2983f7e3223d65d7588c47c
Achrafsadeq <achrafsadeqq@gmail.com>
2024-06-26 13:03:54 -0700

    Add test file 0-main.c and header main.h to test positive_or_negative function with input 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
-* main - Tests function that prints if integer is positive or negative
-* Description: This function initializes an integer to 0 and then calls
-* the function positive_or_negative to check if the integer is positive
-* or negative.
-* Return: 0 on success
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
+
 int main(void)
 {
 	int i;
 
-	i = 0; /* Initialize i to 0 */
-	positive_or_negative(i); /* Check if i is positive or negative */
+	i = 0;
+	positive_or_negative(i);
 
-	return (0); /* Return 0 to indicate successful execution */
+	return (0);
 }
