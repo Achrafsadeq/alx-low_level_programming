5efc6a3c0d028d40ef69262feff1708bd4df491d
Achrafsadeq <achrafsadeqq@gmail.com>
2024-07-01 06:47:04 -0700

    Implement function to convert a string to an integer, handling various edge cases and numeric representations

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to convert
+ *
+ * Description: Parses string, handles signs, converts digits to integer value.
+ * Return: the converted integer
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign = sign * -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+		}
+		else if (result != 0)
+		{
+			break;
+		}
+		i++;
+	}
+	return (result * sign);
+}
