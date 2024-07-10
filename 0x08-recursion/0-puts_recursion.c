fdef877b55243e890b354e4b183357cc3e186c9f
Achrafsadeq <achrafsadeqq@gmail.com>
2024-07-10 09:33:32 -0700

    Write function to print string recursively

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,17 +3,14 @@
 /**
  * _puts_recursion - Prints a string, followed by a new line.
  * @s: The string to be printed.
- *
- * Description: Recursively prints each character of a string
- * followed by a new line.
  */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		_putchar('\n');
-		return; /* End of string reached */
+		return;
 	}
-	_putchar(*s); /* Print current character */
-	_puts_recursion(s + 1); /* Recursive call with next character */
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
