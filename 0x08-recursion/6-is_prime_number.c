6a999f5bc1304049f0a32dd58c43266adf06a46f
Achrafsadeq <achrafsadeqq@gmail.com>
2024-07-10 13:19:49 -0700

    Write recursive function to check if number is prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,7 @@
 #include "main.h"
+
+int check_nmber_is_prime(int n, int i);
+
 /**
  * is_prime_number - Checks if a number is prime
  * @n: The number to check
@@ -8,20 +11,20 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (check_prime(n, 2));
+	return (check_nmber_is_prime(n, 2));
 }
 
 /**
- * check_prime - Recursively checks if a number is prime
+ * check_nmber_is_prime - Recursively checks if a number is prime
  * @n: The number to check
  * @i: The current divisor to check
  * Return: 1 if prime, 0 otherwise
  */
-int check_prime(int n, int i)
+int check_nmber_is_prime(int n, int i)
 {
 	if (i * i > n)
 		return (1);
 	if (n % i == 0)
 		return (0);
-	return (check_prime(n, i + 1));
+	return (check_nmber_is_prime(n, i + 1));
 }
