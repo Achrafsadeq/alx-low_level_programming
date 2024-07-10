045e7eef6d4d6802603a51575b29394efa80aa5f
Achrafsadeq <achrafsadeqq@gmail.com>
2024-07-10 09:36:44 -0700

    Write function to print string recursively

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,11 +6,13 @@
  */
 void _puts_recursion(char *s)
 {
+	/* Base case: if current character is null, we've reached the end */
 	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
+	/* Recursive case: count this character and move to the next */
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
