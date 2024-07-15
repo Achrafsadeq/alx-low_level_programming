464c05f838bc308e083adfbc98b4d24e480d1d71
Achrafsadeq <achrafsadeqq@gmail.com>
2024-07-15 11:12:06 -0700

    Free 2D grid created by alloc_grid function. Ensure proper deallocation of memory.

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+/**
+ * release_grid - Frees a 2 dimensional grid previously created by alloc_grid
+ * @grid: The address of the two dimensional grid
+ * @rows: Height of the grid
+ *
+ * Return: Nothing
+ */
+void release_grid(int **grid, int rows)
+{
+	int index;
+
+	/* Check if the grid is NULL or rows is less than or equal to 0 */
+	if (grid == NULL || rows <= 0)
+		return;
+
+	/* Free each row of the grid */
+	for (index = 0; index < rows; index++)
+	{
+		free(grid[index]);
+	}
+
+	/* Free the grid itself */
+	free(grid);
+}
