da217ccf247ff7ab47814756fae208cf19f458b1
achrafsadeq <achrafsadeqq@gmail.com>
2024-08-01 12:53:13 -0700

    Add constructor function to print message before main executes

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* Function prototype with constructor attribute */
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - function that prints a message before main is executed
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
