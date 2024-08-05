9f844bbece5e12bf8efb7925b38957886fe36393
achrafsadeq <achrafsadeqq@gmail.com>
2024-08-05 14:47:25 -0700

    Free a listint_t linked list safely (handling loops).

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,40 +2,40 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - Frees a listint_t list safely (can free lists with a loop)
+ * free_listint_safe - Frees a listint_t list safely (can free lists with loop)
  * @h: Pointer to the pointer of the start of the list
  *
  * Return: The size of the list that was freed
  */
 size_t free_listint_safe(listint_t **h)
 {
-    size_t len = 0;
-    int diff;
-    listint_t *temp;
+	size_t len = 0;
+	int diff;
+	listint_t *temp;
 
-    if (!h || !*h)
-        return (0);
+	if (!h || !*h)
+		return (0);
 
-    while (*h)
-    {
-        diff = *h - (*h)->next;
-        if (diff > 0)
-        {
-            temp = (*h)->next;
-            free(*h);
-            *h = temp;
-            len++;
-        }
-        else
-        {
-            free(*h);
-            *h = NULL;
-            len++;
-            break;
-        }
-    }
+	while (*h)
+	{
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			len++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
+		}
+	}
 
-    *h = NULL;
+	*h = NULL;
 
-    return (len);
+	return (len);
 }
