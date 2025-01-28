0dde3d5af626d32cce8b3a4e16778951a5147da2
Achrafsadeq <achrafsadeqq@gmail.com>
2025-01-28 14:03:17 -0800

    Implement a jump search algorithm for a sorted singly linked list.

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,13 +1,15 @@
 #include "search_algos.h"
 #include <math.h>
+#include <stdio.h>
 
 /**
- * jump_list - Searches for value in sorted singly linked list
- *             using Jump search
- * @list: Pointer to head of list
- * @size: Number of nodes in list
- * @value: Value to search for
- * Return: Pointer to first node where value is located, or NULL if not found
+ * jump_list - Searches for a value in a sorted singly linked list
+ *             using the Jump search algorithm.
+ * @list: Pointer to the head of the list.
+ * @size: Number of nodes in the list.
+ * @value: Value to search for.
+ * Return: Pointer to the first node where value is located,
+ *         or NULL if the value is not found or the list is NULL.
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
@@ -16,14 +18,15 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 
 	if (list == NULL || size == 0)
 		return (NULL);
-
 	step = sqrt(size);
 	current = list;
 	prev = NULL;
-	while (current->index < size && current->n < value)
+
+	while (current && current->index < size && current->n < value)
 	{
 		prev = current;
 		step_size = 0;
+
 		while (current->next && step_size < step)
 		{
 			current = current->next;
@@ -34,13 +37,14 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 	printf("Value found between indexes [%lu] and [%lu]\n",
 		prev ? prev->index : 0, current->index);
 	current = prev ? prev : list;
-	while (current && current->index <= size &&
-		(prev == NULL || current->index <= prev->index + step))
+	while (current && current->index <= (prev->index + step))
 	{
 		printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
 		if (current->n == value)
 			return (current);
 		current = current->next;
 	}
+
 	return (NULL);
 }
+
